e62b33f0ce39af00dab2e8656778dcee71e13cd1
Henry Yuan <19hy3@queensu.ca>
2022-07-04 01:52:19 -0400

    first commit

---
+++ Week 0 - Functions/hello.py
@@ -0,0 +1,5 @@
+# Ask the user for their name and remove whitespace from str and capitalize user's name 
+name = input("What's your name? ").strip().title() 
+
+# Say hello to user 
+print(f"hello, {name}")
