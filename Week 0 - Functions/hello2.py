1c9eafc192dea37ae9ca753b9f6fb6ef8a489b72
Henry Yuan <19hy3@queensu.ca>
2022-07-04 02:04:20 -0400

    first commit

---
+++ Week 0 - Functions/hello2.py
@@ -0,0 +1,9 @@
+# Define the hello() function 
+def hello(to): 
+    print("hello,", to) 
+
+# Ask the user for their name
+name = input("What's your name? ") 
+
+# Call hello() function 
+hello(name) 
