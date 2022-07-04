0ad344860d93a371ce774f0c7201d2b4bcb39ce8
Henry Yuan <19hy3@queensu.ca>
2022-07-04 14:14:38 -0400

    first commit

--- Week 0 - Functions/calculator2.py
+++ Week 0 - Functions/calculator2.py
@@ -1,8 +1,14 @@
+# Define main function 
 def main(): 
+    # Asking user for input 
     x = int(input("What's x? "))
+
+    # Squaring the user's input 
     print("x squared is", square(x))
 
+# Define square(n) function 
 def square(n): 
     return n * n 
 
+# Call main() function 
 main() 
