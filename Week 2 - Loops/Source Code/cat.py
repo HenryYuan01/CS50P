d5a3aff743e13bd33f9f21b48606928c314bd4b4
Henry Yuan <19hy3@queensu.ca>
2022-07-05 17:25:30 -0400

    cat

--- Week 2 - Loops/Source Code/cat.py
+++ Week 2 - Loops/Source Code/cat.py
@@ -13,4 +13,35 @@ for _ in range(3):
 '''
 
 # More pythonic 
+''' 
 print("meow\n" * 3, end="")
+'''
+
+# Ask user for number of meows 
+'''
+while True: 
+    n = int(input("What's n? "))
+    if n > 0: 
+        break
+
+for _ in range(n): 
+    print("meow")
+'''
+
+# In main function 
+def main(): 
+    number = get_number() 
+    meow(number) 
+
+def get_number(): 
+    while True: 
+        n = int(input("what's n? "))
+        if n > 0: 
+            break 
+    return n
+
+def meow(n): 
+    for _ in range(n): 
+        print("meow")
+
+main() 
