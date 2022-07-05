46c679bf9a39f5b481a3ba78fe2e4a4667db3c34
Henry Yuan <19hy3@queensu.ca>
2022-07-05 17:16:03 -0400

    cat

--- Week 2 - Loops/Source Code/cat.py
+++ Week 2 - Loops/Source Code/cat.py
@@ -1,4 +1,16 @@
+# While loop 
+'''
 i = 0 
 while i < 3: 
     print("meow") 
     i += 1 
+'''
+
+# For loop 
+'''
+for _ in range(3): 
+    print("meow")
+'''
+
+# More pythonic 
+print("meow\n" * 3, end="")
