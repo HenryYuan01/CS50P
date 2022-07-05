a2a44cda5120916c7ff4223e6a630e392b371425
Henry Yuan <19hy3@queensu.ca>
2022-07-05 19:34:03 -0400

    hogwarts

--- Week 2 - Loops/Source Code/hogwarts.py
+++ Week 2 - Loops/Source Code/hogwarts.py
@@ -1,4 +1,36 @@
+'''
 students = ["Hermione", "Harry", "Ron"]
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+'''
 
+# For loop 
+'''
 for student in students: 
     print(student)
+'''
+
+# Length 
+''' 
+for i in range(len(students)): 
+    print(i + 1, students[i])
+'''
+
+# Dictionary 
+students = {
+    "Hermione": "Gryffindor", 
+    "Harry": "Gryffindor", 
+    "Ron": "Gryffindor", 
+    "Draco": "Slytherin"
+}
+
+# Hardcoded 
+'''
+print(students["Hermione"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
+'''
+
+# Dynamically 
+for student in students: 
+    print(student, students[student], sep=": ")
