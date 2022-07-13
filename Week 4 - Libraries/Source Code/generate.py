794aaa0b67215a68b21350354a864b6700de7304
Henry Yuan <19hy3@queensu.ca>
2022-07-13 19:48:50 -0400

    generate

--- Week 4 - Libraries/Source Code/generate.py
+++ Week 4 - Libraries/Source Code/generate.py
@@ -1,4 +1,17 @@
 import random 
+# from random import choice 
 
+# coin toss 
 coin = random.choice(["heads", "tails"])
+# coin = choice(["heads", "tails"])
 print(coin)
+
+# pick a random number 
+number = random.randint(1, 10) 
+print(number)
+
+# shuffle cards 
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards: 
+    print(card)
