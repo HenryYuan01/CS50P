397410e41fb18eb826853d2ad814c7213113fabd
Henry Yuan <19hy3@queensu.ca>
2022-08-03 20:15:04 -0400

    calculator and test_calculator

---
+++ Week 5 - Unit Tests/Source Code/test_calculator.py
@@ -0,0 +1,50 @@
+from calculator import square 
+
+''' 
+def main(): 
+    test_square() 
+'''
+
+
+def test_square(): 
+    # standard way of testing 
+    ''' 
+    if square(2) != 4: 
+        print("2 squared was not 4")
+    if square(3) != 9: 
+        print("3 squared was not 9")
+    '''
+    # using assert with try/except 
+    ''' 
+    try: 
+        assert square(2) == 4 
+    except AssertionError: 
+        print("2 squared was not 4")
+    try: 
+        assert square(3) == 9 
+    except AssertionError: 
+        print("3 squared was not 9")
+    try: 
+        assert square(-2) == 4 
+    except AssertionError: 
+        print("-2 squared was not 4")
+    try: 
+        assert square(-3) == 9 
+    except AssertionError: 
+        print("-3 squared was not 4")
+    try: 
+        assert square(0) == 0 
+    except AssertionError: 
+        print("0 squared was not 0")
+    '''
+    # using assert with pytest 
+    assert square(2) == 4 
+    assert square(3) == 9 
+    assert square(-2) == 4 
+    assert square(-3) == 9 
+    assert square(0) == 0 
+    
+''' 
+if __name__ == "__main__": 
+    main() 
+'''
