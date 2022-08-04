2d18b19accaf52c6111afc392fa94aa0f1a9b4e0
Henry Yuan <19hy3@queensu.ca>
2022-08-03 20:36:10 -0400

    test_calculator

--- Week 5 - Unit Tests/Source Code/test_calculator.py
+++ Week 5 - Unit Tests/Source Code/test_calculator.py
@@ -38,10 +38,26 @@ def test_square():
         print("0 squared was not 0")
     '''
     # using assert with pytest 
+    # run using "python -m pytest test_calculator.py"
+    ''' 
     assert square(2) == 4 
     assert square(3) == 9 
     assert square(-2) == 4 
     assert square(-3) == 9 
+    assert square(0) == 0 
+    '''
+
+# three separate functions 
+
+def test_positive(): 
+    assert square(2) == 4 
+    assert square(3) == 9 
+
+def test_negative(): 
+    assert square(-2) == 4 
+    assert square(-3) == 9 
+
+def test_zero(): 
     assert square(0) == 0 
     
 ''' 
