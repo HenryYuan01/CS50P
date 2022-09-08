9ca620b854a2e13be7a93a8818130dc68228a8b5
Henry Yuan <19hy3@queensu.ca>
2022-09-08 16:45:05 -0400

    format

--- Week 7 - Regular Expressions/Source Code/format.py
+++ Week 7 - Regular Expressions/Source Code/format.py
@@ -12,6 +12,7 @@ print(f"hello, {name}")
 
 matches = re.search(r"^(.+), (.+)$", name)
 if matches: 
-    last, first = matches.groups() 
-    name = f"{first} {last}" 
+    # last, first = matches.groups() 
+    # name = f"{first} {last}" 
+    name = matches.group(2) + " " + matches.group(1) 
 print(f"hello, {name}")
