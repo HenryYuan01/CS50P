1081bc0ae426a2c71f97edd28db7273026616356
Henry Yuan <19hy3@queensu.ca>
2022-09-08 16:42:14 -0400

    format

---
+++ Week 7 - Regular Expressions/Source Code/format.py
@@ -0,0 +1,17 @@
+import re 
+
+name = input("What's your name? ").strip() 
+
+''' 
+# in case user inputs "Yuan, Henry"
+if "," in name: 
+    last, first = name.split(", ") 
+    name = f"{first} {last}" 
+print(f"hello, {name}") 
+'''
+
+matches = re.search(r"^(.+), (.+)$", name)
+if matches: 
+    last, first = matches.groups() 
+    name = f"{first} {last}" 
+print(f"hello, {name}")
